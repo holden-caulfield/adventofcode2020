b814020f12ee6d7fe195b9685d3616315c73b41a
JP Saraceno <jp@scvsoft.com>
2020-12-04 11:33:11 -0300

    day 2 puzzles

---
+++ day2/puzzle1.js
@@ -0,0 +1,11 @@
+const data = require("./data.json")
+const R = require("ramda")
+
+const isValid = ({min, max, letter, password}) => {
+    const letters = [password.charAt(min-1), password.charAt(max-1)]
+    return R.filter(R.equals(letter), letters).length === 1
+}
+
+const valid = data.filter(isValid)
+
+console.log(valid.length)

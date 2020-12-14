2aa1c31d0876a3924c6564962839088311001ef3
JP Saraceno <jp@scvsoft.com>
2020-12-14 13:36:26 -0300

    solution to day 14

---
+++ day14/puzzle.js
@@ -0,0 +1,83 @@
+const { lineByLine } = require("../utils/rxFile")
+const { map, reduce } = require("rxjs/operators")
+const R = require("ramda")
+
+const FILE = "./data.txt"
+
+const getFreshInitialState = () => ({ memory: {}, mask: "" })
+
+const chainIndexed = R.addIndex(R.chain)
+
+const parseInstruction = (line) => {
+  const [__, operation, address, argument] = line.match(
+    /(\w*)\[?(\d*)?\]? = (.*)/
+  )
+  return { operation, address, argument }
+}
+
+const setBit = (mask, index, bit) => R.join("", R.update(index, bit, mask))
+
+const findAllFloating = (mask) =>
+  R.filter((index) => mask[index] === "X", R.range(0, mask.length))
+
+const maskValue = (value, mask) => {
+  const maskForZeroes = BigInt(parseInt(mask.replace(/X/g, "1"), 2))
+  const maskForOnes = BigInt(parseInt(mask.replace(/X/g, "0"), 2))
+  return (BigInt(value) | maskForOnes) & maskForZeroes
+}
+
+const maskAddresses = (address, floatingMask) => {
+  const expandedMasks = R.reduce(
+    (masks, index) =>
+      R.chain(
+        (mask) => [setBit(mask, index, 0), setBit(mask, index, 1)],
+        masks
+      ),
+    [floatingMask.replace(/0/g, "X")],
+    findAllFloating(floatingMask)
+  )
+  return expandedMasks.map((mask) => maskValue(address, mask))
+}
+
+const updateByMaskedValue = ({ memory, mask }, address, value) => {
+  memory[address] = maskValue(value, mask)
+  return memory
+}
+
+const updateByMaskedAddresses = ({ memory, mask }, baseAddress, value) => {
+  const addresses = maskAddresses(baseAddress, mask)
+  for (address of addresses) {
+    memory[address.toString()] = value
+  }
+  return memory
+}
+
+const applyInstruction = (updateMemoryMethod) => (
+  state,
+  { operation, address, argument }
+) => {
+  switch (operation) {
+    case "mask":
+      return { ...state, mask: argument }
+    case "mem":
+      return {
+        ...state,
+        memory: updateMemoryMethod(state, address, argument)
+      }
+  }
+}
+
+const instruction$ = (updateMemoryMethod) =>
+  lineByLine(FILE).pipe(
+    map(parseInstruction),
+    reduce(applyInstruction(updateMemoryMethod), getFreshInitialState()),
+    map(({ memory }) => R.sum(Object.values(memory)))
+  )
+
+instruction$(updateByMaskedValue).subscribe((totalSum) =>
+  console.log(`Total sum for case 1 is ${totalSum}`)
+)
+
+instruction$(updateByMaskedAddresses).subscribe((totalSum) =>
+  console.log(`Total sum for case 2 is ${totalSum}`)
+)
